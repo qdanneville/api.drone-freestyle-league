dff9e0e51d7ef2400b057dce0360a178f6aaef91
Quentin Danneville <quentin@qdan.io>
2020-11-06 14:12:50 +0100

    [Profile] Getting a profile type according to a user and a role type

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,8 +1,38 @@
-'use strict';
+module.exports = {
+    find: async (ctx) => {
+        let profile = null;
+        let profiles = [];
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+        if (ctx.url.includes('?user')) {
+            profile = await strapi.api.profile.services.profile.findOne(ctx.query);
+        } else {
+            profiles = await strapi.api.profile.services.profile.find(ctx.query);
+        }
 
-module.exports = {};
+        //The purpose of this is to get a profile Type when a user is trying to find a profile with a matching user
+        if (profile) {
+            const role = await strapi
+                .query('role', 'users-permissions')
+                .findOne({ id: profile.user.role }, []);
+
+            //We're now getting the pilot or brand profile type according to user role
+            if (role) {
+                let profileType = null;
+
+                if (role.type === "pilot") {
+                    profileType = await strapi.api.pilot.services.pilot.findOne({ profile: profile.id })
+                    profileType.pilot_id = profileType.id
+                } else if (role.type === "brand") {
+                    profileType = await strapi.api.brand.services.brand.findOne({ profile: profile.id })
+                    profileType.brand_id = profileType.id
+                }
+
+                if (profileType) return profileType;
+            }
+
+            return profile;
+        }
+
+        return profiles;
+    }
+};
