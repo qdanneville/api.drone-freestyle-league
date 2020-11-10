90507e5587410186496a9a211b7822e275ab566e
Quentin Danneville <quentin@qdan.io>
2020-11-10 14:52:44 +0100

    [Spots] Getting public & pilot spot
    Creating a feature object for each spot.
    A feature object is a mapbox object displayed on the map

--- api/spot/controllers/spot.js
+++ api/spot/controllers/spot.js
@@ -5,4 +5,56 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    getMapSpots: async (ctx) => {
+
+        if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+            try {
+                const { id } = await strapi.plugins[
+                    'users-permissions'
+                ].services.jwt.getToken(ctx);
+
+                const profile = await strapi
+                    .query('profile')
+                    .findOne({ user: id });
+
+                const pilot = await strapi
+                    .query('pilot')
+                    .findOne({ profile: profile.id });
+
+                //Get public spots
+                const publicSpots = await strapi
+                    .query('spot')
+                    .find({ public: true });
+
+                //Get pilot spots
+                const pilotSpots = await strapi
+                    .query('spot')
+                    .find({ pilot: pilot.id, public: false });
+
+
+                //Once we get all spots, we're creating a new feature
+                //A feature is a mapbox feature object in order to be displayed on the map
+                const publicSpotsFeatures = publicSpots.map(spot => {
+                    return strapi.services.spot.createSpotFeature(spot)
+                })
+
+                const pilotSpotsFeatures = pilotSpots.map(spot => {
+                    return strapi.services.spot.createSpotFeature(spot)
+                })
+
+                if (publicSpotsFeatures && pilotSpotsFeatures) return { publicSpotsFeatures, pilotSpotsFeatures }
+            }
+
+            catch (err) {
+                return ctx.badRequest(
+                    null,
+                    formatError({
+                        id: 'Spot.map.prohibited',
+                        message: "Impossible to find spots.",
+                    })
+                );
+            }
+        }
+    }
+};
--- api/spot/services/spot.js
+++ api/spot/services/spot.js
@@ -5,4 +5,21 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    createSpotFeature: (spot) => {
+
+        //A feature is a mapbox feature object
+        //Used to display a spot as a marker (feature) on the map
+        //https://docs.mapbox.com/help/glossary/features/
+        const spotFeature = {
+            "type": "Feature",
+            "properties": spot,
+            "geometry": {
+                "coordinates": [spot.longitude, spot.latitude],
+                "type": "Point"
+            }
+        }
+
+        return spotFeature
+    }
+};
