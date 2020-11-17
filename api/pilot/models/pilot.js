b902a91f5ce2b46cbc39a094985506b42e35dcee
Quentin Danneville <quentin@qdan.io>
2020-11-17 18:20:19 +0100

    Added level 1 by default to each pilot when they’re created

--- api/pilot/models/pilot.js
+++ api/pilot/models/pilot.js
@@ -1,8 +1,12 @@
 'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+//A pilot is level 1 when he starts
+//And has 0 experience (current_points)
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            const defaultLevel = await strapi.query('level').findOne({ number: 1 })
+            if (defaultLevel) data.level = defaultLevel
+            data.current_points = 0.00;
+        }
+    }
+};
