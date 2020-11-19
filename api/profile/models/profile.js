1d8a592c6941961ac5fa283e0b4f1bf779ac8c11
Quentin Danneville <quentin@qdan.io>
2020-11-19 18:49:21 +0100

    [Profile] Added a slug calculated with the user username

--- api/profile/models/profile.js
+++ api/profile/models/profile.js
@@ -1,8 +1,17 @@
 'use strict';
 
+
+const slugify = require('slugify');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            if (data.user && data.user.username) data.slug = slugify(data.user.username);
+        }
+    },
+};
