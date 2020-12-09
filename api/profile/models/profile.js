79f812c739112ec350b15c4e28bda97bdd31cfc6
Quentin Danneville <quentin@qdan.io>
2020-12-09 15:43:38 +0100

    Default profile image added

--- api/profile/models/profile.js
+++ api/profile/models/profile.js
@@ -2,6 +2,7 @@
 
 
 const slugify = require('slugify');
+const axios = require('axios');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
@@ -12,6 +13,8 @@ module.exports = {
     lifecycles: {
         async beforeCreate(data) {
             if (data.user && data.user.username) data.slug = slugify(data.user.username);
+            const ImageResponse = await axios.get('http://localhost:1337/upload/files?name=default-profile-image')
+            if (ImageResponse && ImageResponse.data) data.avatar = ImageResponse.data
         }
     },
 };
