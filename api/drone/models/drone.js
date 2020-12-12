5eb0a95a8721eaa2cbaddd368008de1bd5ec752a
Quentin Danneville <quentin@qdan.io>
2020-12-12 20:11:08 +0100

    [Gear] WIP

--- api/drone/models/drone.js
+++ api/drone/models/drone.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const slugify = require('slugify');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            if (data.name) data.slug = slugify(data.name);
+        },
+        async beforeUpdate(data, attrs) {
+            if (attrs.name) attrs.slug = slugify(attrs.name);
+        },
+    },
+};
--- api/pilot-gear/controllers/pilot-gear.js
+++ api/pilot-gear/controllers/pilot-gear.js
@@ -5,4 +5,51 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    findPilotGear: async (ctx) => {
+        if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+
+            let pilotId = null;
+
+            //If a user requests his own gears
+            if (!ctx.query.pilot) {
+                const { id } = await strapi.plugins[
+                    'users-permissions'
+                ].services.jwt.getToken(ctx);
+
+                const fetchProfile = await strapi
+                    .query('profile')
+                    .findOne({ user: id }, ['id']);
+
+                const fetchPilot = await strapi
+                    .query('pilot')
+                    .findOne({ profile: fetchProfile.id }, ['id']);
+
+                if (!fetchPilot) {
+                    return ctx.unauthorized(`Can't find this pilot`);
+                }
+
+                pilotId = fetchPilot.id
+            } else {
+                //If a user requests an other pilot gears
+                pilotId = ctx.query.pilot
+            }
+
+            const url = ctx.request.url;
+            let entities = []
+
+            if (url.includes('accessories')) {
+                entities = await strapi.query('pilot-gear').findOne({ pilot: pilotId }, ['gears', 'gears.image','gears.gear_type','gears.manufacturer'])
+                entities = entities ? entities.gears : []
+            } else if (url.includes('drones')) {
+                entities = await strapi.query('pilot-gear').findOne({ pilot: pilotId }, ['drones', 'drones.image'])
+                entities = entities ? entities.drones : []
+            } else if (url.includes('batteries')) {
+                entities = await strapi.query('pilot-gear').findOne({ pilot: pilotId }, ['batteries', 'batteries.image'])
+                entities = entities ? entities.batteries : []
+            }
+
+            return entities
+        }
+    }
+};
