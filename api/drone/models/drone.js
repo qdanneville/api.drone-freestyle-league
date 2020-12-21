aa7a86f68db0d140d51194564b12d3b80938229c
Quentin Danneville <quentin@qdan.io>
2020-12-21 15:41:31 +0100

    [Drone] Default image

--- api/drone/models/drone.js
+++ api/drone/models/drone.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const slugify = require('slugify');
+const axios = require('axios');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
@@ -13,6 +14,10 @@ module.exports = {
         async afterCreate(data) {
             if (data.name) data.slug = slugify(data.name + '-' + data.id);
             await strapi.query('drone').update({ id: data.id }, { slug: data.slug })
+        },
+        async beforeCreate(data) {
+            const ImageResponse = await axios.get('http://localhost:1337/upload/files?name=default-drone-image')
+            if (ImageResponse && ImageResponse.data) data.image = ImageResponse.data
         },
         async beforeUpdate(data, attrs) {
             if (attrs && attrs.name && data && data.id) attrs.slug = slugify(attrs.name + '-' + data.id);
